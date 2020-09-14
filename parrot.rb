13ebaae57622ca9300e5d0bed28b46835da2aa99
sensible-partial-6157 <daniyellaharmon@gmail.com>
2020-09-14 06:42:23 +0000

    Automatically backed up by Learn

--- parrot.rb
+++ parrot.rb
@@ -1,12 +1,8 @@
-def parrot (language:"hello!"", animal:"bird")
+def parrot (language:"hello!", animal:"bird")
   puts language
   puts animal
 end 
 
 parrot language:"Squawk!", animal: "bird"
 
-def method_with_return
-  retun "hello!"
-end
 
-method_with_return
