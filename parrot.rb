ce7579709654bf308f920e6f27861a120ec63121
sensible-partial-6157 <daniyellaharmon@gmail.com>
2020-09-14 06:56:51 +0000

    Automatically backed up by Learn

--- parrot.rb
+++ parrot.rb
@@ -1,10 +1,3 @@
-#def parrot (language:"Pretty bird!", animal:"bird")
- # puts language
- # puts animal
-#end 
-
-#parrot language:"Squawk!", animal: "bird"
-
 def parrot(string="Squawk!")
   puts "#{string}"
   "#{string}"
